e7e94293befb9acb620f55b7647b6692e38ca031
unknown <77588948+deivmaik@users.noreply.github.com>
2021-10-01 18:05:23 -0400

    chore: Add initial configs.

---
+++ components/Intro.vue
@@ -0,0 +1,11 @@
+<!-- Please remove this file from your project -->
+<template>
+  <h1>Portfolio</h1>
+</template>
+
+<style scoped>
+  h1 {
+    font-family: prompt;
+  }
+</style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <Intro/>
+</template>
+
+<script>
+export default {}
+</script>
